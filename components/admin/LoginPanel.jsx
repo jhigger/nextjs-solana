70bfe7694974dc877f40fbca505d31e6d88b7c9d
jhigger <njohnjhigger@gmail.com>
2022-09-16 16:42:46 +0800

    added login panel

---
+++ components/admin/LoginPanel.jsx
@@ -0,0 +1,98 @@
+import {
+	Button,
+	FormControl,
+	Heading,
+	Input,
+	Stack,
+	useColorModeValue,
+	useToast
+} from '@chakra-ui/react';
+import {useForm} from 'react-hook-form';
+
+const LoginPanel = ({handleLogin}) => {
+	const {
+		handleSubmit,
+		register,
+		formState: {errors, isSubmitting},
+		reset
+	} = useForm();
+
+	const toast = useToast();
+
+	function onSubmit(values) {
+		const showToast = () => {
+			return toast({
+				title: `Logged In`,
+				status: 'info',
+				isClosable: true
+			});
+		};
+
+		if (values.user === 'admin' && values.pass === 'admin') {
+			showToast();
+			reset();
+			handleLogin(true);
+			return;
+		}
+
+		return toast({
+			title: `Incorrect Credentials`,
+			status: 'error',
+			isClosable: true
+		});
+	}
+
+	return (
+		<form onSubmit={handleSubmit(onSubmit)}>
+			<Stack
+				p={8}
+				shadow="md"
+				bg={useColorModeValue('gray.50', 'gray.900')}
+				borderWidth="1px"
+				borderRadius="lg"
+				spacing={4}
+			>
+				<Heading as="h1" align="center">
+					Login
+				</Heading>
+
+				<FormControl isInvalid={errors.user} isRequired>
+					<Input
+						placeholder="Username"
+						id="user"
+						type="text"
+						{...register('user', {
+							required: 'This is required'
+						})}
+					/>
+					{/* <FormErrorMessage>
+					{errors.user && errors.user}
+				</FormErrorMessage> */}
+				</FormControl>
+
+				<FormControl isInvalid={errors.pass} isRequired>
+					<Input
+						placeholder="Password"
+						id="pass"
+						type="password"
+						{...register('pass', {
+							required: 'This is required'
+						})}
+					/>
+					{/* <FormErrorMessage>
+					{errors.pass && errors.pass}
+				</FormErrorMessage> */}
+				</FormControl>
+				<Button
+					colorScheme="purple"
+					isLoading={isSubmitting}
+					type="submit"
+				>
+					Login
+				</Button>
+			</Stack>
+		</form>
+	);
+};
+
+export default LoginPanel;
--- pages/admin.js
+++ pages/admin.js
@@ -1,11 +1,17 @@
 import {Container, Flex, Stack} from '@chakra-ui/react';
 import Head from 'next/head';
 import {useEffect, useState} from 'react';
+import LoginPanel from '../components/admin/LoginPanel';
 import SubmissionsTable from '../components/admin/SubmissionsTable';
 
 const admin = () => {
 	const [submissions, setSubmissions] = useState([]);
 	const [refresh, toggleRefresh] = useState(false);
+	const [loggedIn, setLoggedIn] = useState(false);
+
+	const handleLogin = (value) => {
+		setLoggedIn(value);
+	};
 
 	const handleRefresh = () => {
 		toggleRefresh((prev) => !prev);
@@ -29,14 +35,20 @@ const admin = () => {
 				<title>Admin Dashboard</title>
 			</Head>
 			<Flex as="main" minH="100vh" justify="center">
-				<Container maxW="container.lg" py={4}>
-					<Stack spacing={4}>
-						<SubmissionsTable
-							submissions={submissions}
-							handleRefresh={handleRefresh}
-						/>
-					</Stack>
-				</Container>
+				{loggedIn ? (
+					<Container maxW="container.lg" py={4}>
+						<Stack spacing={4}>
+							<SubmissionsTable
+								submissions={submissions}
+								handleRefresh={handleRefresh}
+							/>
+						</Stack>
+					</Container>
+				) : (
+					<Container maxW="xs" py={4}>
+						<LoginPanel handleLogin={handleLogin} />
+					</Container>
+				)}
 			</Flex>
 		</>
 	);
