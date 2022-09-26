8a41cd52e0fe548f66beb5d2798a0d485adb8997
jhigger <njohnjhigger@gmail.com>
2022-09-27 01:39:07 +0800

    edit

--- components/admin/ApprovedTable.jsx
+++ components/admin/ApprovedTable.jsx
@@ -87,77 +87,6 @@ const LinkInput = ({ address, link, handleRefresh }) => {
 	);
 };
 
-const Approved = ({ approved }) => {
-	return (
-		<TableContainer w="full" h="full" overflowY>
-			<Table variant="striped" colorScheme="gray">
-				<Thead>
-					<Tr>
-						<Th>Discord ID</Th>
-						<Th>Community Name</Th>
-						<Th>Discord Server</Th>
-						<Th>Twitter </Th>
-						<Th>Service</Th>
-						<Th textAlign="center">Bot Link</Th>
-					</Tr>
-				</Thead>
-				<Tbody>
-					{approved.map((row) => {
-						return (
-							<Tr key={row.address}>
-								<Td>
-									<Button
-										rightIcon={<FaCopy />}
-										variant="link"
-										onClick={() =>
-											handleCopy(row?.discordId)
-										}
-									>
-										{row?.discordId}
-									</Button>
-								</Td>
-								<Td>{row?.communityName}</Td>
-								<Td>
-									<Link
-										href={`//${row?.discordServerUrl}`}
-										isExternal
-									>
-										<IconButton
-											aria-label="Discord server URL"
-											icon={<FaDiscord />}
-											variant="ghost"
-										/>
-									</Link>
-								</Td>
-								<Td>
-									<Link
-										href={`//${row?.twitterUrl}`}
-										isExternal
-									>
-										<IconButton
-											aria-label="Twitter URL"
-											icon={<FaTwitter />}
-											variant="ghost"
-										/>
-									</Link>
-								</Td>
-								<Td>{row?.service}</Td>
-								<Td>
-									<LinkInput
-										address={row.address}
-										link={row?.link}
-										handleRefresh={handleRefresh}
-									/>
-								</Td>
-							</Tr>
-						);
-					})}
-				</Tbody>
-			</Table>
-		</TableContainer>
-	);
-};
-
 const ApprovedTable = ({ approved, isLoading, handleRefresh }) => {
 	const toast = useToast();
 
@@ -195,7 +124,72 @@ const ApprovedTable = ({ approved, isLoading, handleRefresh }) => {
 					<Spinner />
 				</Center>
 			) : (
-				<Approved approved={approved} />
+				<TableContainer w="full" h="full" overflowY>
+					<Table variant="striped" colorScheme="gray">
+						<Thead>
+							<Tr>
+								<Th>Discord ID</Th>
+								<Th>Community Name</Th>
+								<Th>Discord Server</Th>
+								<Th>Twitter </Th>
+								<Th>Service</Th>
+								<Th textAlign="center">Bot Link</Th>
+							</Tr>
+						</Thead>
+						<Tbody>
+							{approved.map((row) => {
+								return (
+									<Tr key={row.address}>
+										<Td>
+											<Button
+												rightIcon={<FaCopy />}
+												variant="link"
+												onClick={() =>
+													handleCopy(row?.discordId)
+												}
+											>
+												{row?.discordId}
+											</Button>
+										</Td>
+										<Td>{row?.communityName}</Td>
+										<Td>
+											<Link
+												href={`//${row?.discordServerUrl}`}
+												isExternal
+											>
+												<IconButton
+													aria-label="Discord server URL"
+													icon={<FaDiscord />}
+													variant="ghost"
+												/>
+											</Link>
+										</Td>
+										<Td>
+											<Link
+												href={`//${row?.twitterUrl}`}
+												isExternal
+											>
+												<IconButton
+													aria-label="Twitter URL"
+													icon={<FaTwitter />}
+													variant="ghost"
+												/>
+											</Link>
+										</Td>
+										<Td>{row?.service}</Td>
+										<Td>
+											<LinkInput
+												address={row.address}
+												link={row?.link}
+												handleRefresh={handleRefresh}
+											/>
+										</Td>
+									</Tr>
+								);
+							})}
+						</Tbody>
+					</Table>
+				</TableContainer>
 			)}
 		</Stack>
 	);
--- components/admin/PendingTable.jsx
+++ components/admin/PendingTable.jsx
@@ -31,103 +31,6 @@ import { useState } from 'react';
 import { FaCopy, FaDiscord, FaTwitter } from 'react-icons/fa';
 import RejectReasonModal from './RejectReasonModal';
 
-const Pending = ({ pending }) => {
-	return (
-		<TableContainer w="full" h="full" overflowY>
-			<Table variant="striped" colorScheme="gray" size="md">
-				<Thead>
-					<Tr>
-						<Th>Discord ID</Th>
-						<Th>Community Name</Th>
-						<Th>Discord Server</Th>
-						<Th>Twitter </Th>
-						<Th>Service</Th>
-						<Th textAlign="center">Options</Th>
-					</Tr>
-				</Thead>
-				<Tbody>
-					{pending.map((row) => {
-						return (
-							<Tr key={row.address}>
-								<Td>
-									<Button
-										rightIcon={<FaCopy />}
-										variant="link"
-										onClick={() =>
-											handleCopy(row?.discordId)
-										}
-									>
-										{row?.discordId}
-									</Button>
-								</Td>
-								<Td>{row?.communityName}</Td>
-								<Td>
-									<Link
-										href={`//${row?.discordServerUrl}`}
-										isExternal
-									>
-										<IconButton
-											aria-label="Discord server URL"
-											icon={<FaDiscord />}
-											variant="ghost"
-										/>
-									</Link>
-								</Td>
-								<Td>
-									<Link
-										href={`//${row?.twitterUrl}`}
-										isExternal
-									>
-										<IconButton
-											aria-label="Twitter URL"
-											icon={<FaTwitter />}
-											variant="ghost"
-										/>
-									</Link>
-								</Td>
-								<Td>{row?.service}</Td>
-								<Td>
-									<Menu>
-										<Center>
-											<MenuButton
-												as={IconButton}
-												aria-label="Options"
-												icon={
-													loading ? (
-														<Spinner size="sm" />
-													) : (
-														<HamburgerIcon />
-													)
-												}
-												variant="ghost"
-											/>
-										</Center>
-										<MenuList>
-											<MenuItem
-												icon={<CheckIcon />}
-												onClick={() =>
-													handleApprove(row.address)
-												}
-											>
-												Approve
-											</MenuItem>
-											<RejectReasonModal
-												icon={<CloseIcon />}
-												handleReject={handleReject}
-												address={row.address}
-											/>
-										</MenuList>
-									</Menu>
-								</Td>
-							</Tr>
-						);
-					})}
-				</Tbody>
-			</Table>
-		</TableContainer>
-	);
-};
-
 const PendingTable = ({ pending, isLoading, handleRefresh }) => {
 	const toast = useToast();
 	const [loading, setLoading] = useState(false);
@@ -198,7 +101,102 @@ const PendingTable = ({ pending, isLoading, handleRefresh }) => {
 					<Spinner />
 				</Center>
 			) : (
-				<Pending pending={pending} />
+				<TableContainer w="full" h="full" overflowY>
+					<Table variant="striped" colorScheme="gray" size="md">
+						<Thead>
+							<Tr>
+								<Th>Discord ID</Th>
+								<Th>Community Name</Th>
+								<Th>Discord Server</Th>
+								<Th>Twitter </Th>
+								<Th>Service</Th>
+								<Th textAlign="center">Options</Th>
+							</Tr>
+						</Thead>
+						<Tbody>
+							{pending.map((row) => {
+								return (
+									<Tr key={row.address}>
+										<Td>
+											<Button
+												rightIcon={<FaCopy />}
+												variant="link"
+												onClick={() =>
+													handleCopy(row?.discordId)
+												}
+											>
+												{row?.discordId}
+											</Button>
+										</Td>
+										<Td>{row?.communityName}</Td>
+										<Td>
+											<Link
+												href={`//${row?.discordServerUrl}`}
+												isExternal
+											>
+												<IconButton
+													aria-label="Discord server URL"
+													icon={<FaDiscord />}
+													variant="ghost"
+												/>
+											</Link>
+										</Td>
+										<Td>
+											<Link
+												href={`//${row?.twitterUrl}`}
+												isExternal
+											>
+												<IconButton
+													aria-label="Twitter URL"
+													icon={<FaTwitter />}
+													variant="ghost"
+												/>
+											</Link>
+										</Td>
+										<Td>{row?.service}</Td>
+										<Td>
+											<Menu>
+												<Center>
+													<MenuButton
+														as={IconButton}
+														aria-label="Options"
+														icon={
+															loading ? (
+																<Spinner size="sm" />
+															) : (
+																<HamburgerIcon />
+															)
+														}
+														variant="ghost"
+													/>
+												</Center>
+												<MenuList>
+													<MenuItem
+														icon={<CheckIcon />}
+														onClick={() =>
+															handleApprove(
+																row.address
+															)
+														}
+													>
+														Approve
+													</MenuItem>
+													<RejectReasonModal
+														icon={<CloseIcon />}
+														handleReject={
+															handleReject
+														}
+														address={row.address}
+													/>
+												</MenuList>
+											</Menu>
+										</Td>
+									</Tr>
+								);
+							})}
+						</Tbody>
+					</Table>
+				</TableContainer>
 			)}
 		</Stack>
 	);
