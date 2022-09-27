9c7dc4e5b8145c27c9ec9f537e370364de916306
jhigger <njohnjhigger@gmail.com>
2022-09-27 21:45:26 +0800

    added favicons

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,16 +3,43 @@ import NextDocument, { Head, Html, Main, NextScript } from 'next/document';
 import theme from '../theme';
 
 export default class Document extends NextDocument {
-  render() {
-    return (
-      <Html lang="en">
-        <Head />
-        <body>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    );
-  }
+	render() {
+		return (
+			<Html lang="en">
+				<Head>
+					<meta
+						name="description"
+						content="Utility APE: Leading Engage-To-Earn and Social Web3 tools Community, Utility, Security."
+					/>
+					<link rel="icon" type="image/x-icon" href="/favicon.ico" />
+
+					<link
+						rel="apple-touch-icon"
+						sizes="180x180"
+						href="/apple-touch-icon.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="32x32"
+						href="/favicon-32x32.png"
+					/>
+					<link
+						rel="icon"
+						type="image/png"
+						sizes="16x16"
+						href="/favicon-16x16.png"
+					/>
+					<link rel="manifest" href="/site.webmanifest" />
+				</Head>
+				<body>
+					<ColorModeScript
+						initialColorMode={theme.config.initialColorMode}
+					/>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
