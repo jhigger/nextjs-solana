afd8037d3a95ae32de69588c35d7e8609007c877
jhigger <njohnjhigger@gmail.com>
2022-10-01 02:05:09 +0800

    added delete

--- pages/api/submissions/[address]/index.js
+++ pages/api/submissions/[address]/index.js
@@ -11,11 +11,19 @@ export default async (req, res) => {
 			});
 			res.status(200).json(result);
 		} else if (req.method === 'PUT') {
+			// Process a PUT request
 			const { address } = req.query;
 			const { statusId, botLink, rejectReason } = req.body;
 			const result = await prisma.submission.update({
 				where: { address },
 				data: { statusId, botLink, rejectReason, updatedAt: new Date() }
+			});
+			res.status(200).json(result);
+		} else if (req.method === 'DELETE') {
+			// Process a DELETE request
+			const { address } = req.query;
+			const result = await prisma.submission.delete({
+				where: { address }
 			});
 			res.status(200).json(result);
 		} else {
