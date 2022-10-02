f0b0883bf1f87f53e3a8274c04fe5878ed783f71
jhigger <njohnjhigger@gmail.com>
2022-10-03 02:03:18 +0800

    log out change wallet

--- hooks/useAdmin.js
+++ hooks/useAdmin.js
@@ -1,8 +1,8 @@
 import useSWR from 'swr';
 import fetcher from '../lib/fetcher';
 
-export default () => {
-	const url = `/api/admins`;
+export default (address = '') => {
+	const url = `/api/admins/${address}`;
 
 	const { data, isValidating, error, mutate } = useSWR(url, fetcher, {
 		refreshInterval: 5 * 60 * 1000, // 5 minute interval
--- pages/admin.jsx
+++ pages/admin.jsx
@@ -1,4 +1,5 @@
-import { signIn, useSession } from 'next-auth/react';
+import { useWallet } from '@solana/wallet-adapter-react';
+import { signIn, signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
 import { useEffect } from 'react';
 import AdminTable from '../components/admin/AdminTable';
@@ -6,15 +7,20 @@ import ApprovedTable from '../components/admin/ApprovedTable';
 import PendingTable from '../components/admin/PendingTable';
 import RejectedTable from '../components/admin/RejectedTable';
 import Sidebar from '../components/admin/Sidebar';
+import useAdmin from '../hooks/useAdmin';
 
 const Admin = () => {
 	const { status } = useSession();
+	const { publicKey } = useWallet();
+	const { admins: admin } = useAdmin(publicKey?.toBase58());
 
 	useEffect(() => {
 		if (status === 'unauthenticated') {
 			signIn();
+		} else if (!admin) {
+			signOut();
 		}
-	}, [status]);
+	}, [status, publicKey]);
 
 	if (status === 'authenticated')
 		return (
